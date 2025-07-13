20ba9bf13baff266dc50ed3e750f66743462bed6
Ubuntu <ubuntu@ip-172-31-0-218.ap-south-1.compute.internal>
2025-07-13 21:07:25 +0000

    Initial calculator app commit

--- index.js
+++ index.js
@@ -11,3 +11,13 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, () => {
+  console.log(`App running at http://localhost:${port}`);
+});
+
